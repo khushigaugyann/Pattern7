104c88de7f1d9ef95ff31caf96eb446e453c7700
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-09 11:19:41 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,10 @@
+public class Pattern7 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
